791247b33d49852e5743ad22d33dfb0940e295e7
MitchellGerdisch <mitchell.gerdisch@jesslex.com>
2020-09-29 15:54:12 -0500

    no-rg-making

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
 
-// Create an Azure Resource Group
-const resourceGroup = new azure.core.ResourceGroup("resourceGroup");
-
 // Create an Azure resource (Storage Account)
 const account = new azure.storage.Account("storage", {
     // The location for the storage account will be derived automatically from the resource group.
-    resourceGroupName: resourceGroup.name,
+    resourceGroupName: "mitch-rg"
     accountTier: "Standard",
     accountReplicationType: "LRS",
 });
