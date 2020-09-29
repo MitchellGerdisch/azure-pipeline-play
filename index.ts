168348a6036d361d057e13d83c42c9de9b005b6d
MitchellGerdisch <mitchell.gerdisch@jesslex.com>
2020-09-29 13:54:42 -0500

    reinitilize

--- index.ts
+++ index.ts
@@ -2,14 +2,12 @@ import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
 
 // Create an Azure Resource Group
-const resourceGroup = new azure.core.ResourceGroup("resourceGroup", {
-    location: "WestUS",
-});
+const resourceGroup = new azure.core.ResourceGroup("resourceGroup");
 
 // Create an Azure resource (Storage Account)
 const account = new azure.storage.Account("storage", {
+    // The location for the storage account will be derived automatically from the resource group.
     resourceGroupName: resourceGroup.name,
-    location: resourceGroup.location,
     accountTier: "Standard",
     accountReplicationType: "LRS",
 });
