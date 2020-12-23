bed8faa91317b1c2be42ca38826ae844de951ef9
MitchellGerdisch <mitch@pulumi.com>
2020-12-23 14:02:05 -0600

    example with createStack

--- index.ts
+++ index.ts
@@ -2,15 +2,17 @@ import * as pulumi from '@pulumi/pulumi';
 import * as azure from '@pulumi/azure';
 
 // Create an Azure Resource Group
-const resourceGroup = new azure.core.ResourceGroup('mitch-rg-');
+const resourceGroup = new azure.core.ResourceGroup('mitch-rg', {
+  location: "centralus"
+});
 
 // Create an Azure resource (Storage Account)
-const account = new azure.storage.Account('storage', {
-  // The location for the storage account will be derived automatically from the resource group.
-  resourceGroupName: resourceGroup.name,
-  accountTier: 'Standard',
-  accountReplicationType: 'LRS',
-});
+// const account = new azure.storage.Account('storage', {
+//   // The location for the storage account will be derived automatically from the resource group.
+//   resourceGroupName: resourceGroup.name,
+//   accountTier: 'Standard',
+//   accountReplicationType: 'LRS',
+// });
 
 // Export the connection string for the storage account
 export const connectionString = account.primaryConnectionString;
