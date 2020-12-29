500503f1bd85f77279a88acba8257e924ef09685
MitchellGerdisch <mitch@pulumi.com>
2020-12-29 15:08:52 -0600

    testing other fields

--- index.ts
+++ index.ts
@@ -2,10 +2,25 @@ import * as pulumi from '@pulumi/pulumi';
 import * as azure from '@pulumi/azure';
 
 const location = process.env["RG_LOCATION"] || "centralus"
+const dbLogin = process.env["DB_LOGIN"] 
+const dbPassword = process.env["DB_PASSWORD"] 
+
 // Create an Azure Resource Group
 export const resourceGroup = new azure.core.ResourceGroup("mitch-devops-rg", {
   location: location
 }, {additionalSecretOutputs: ["location"]});
+
+export const psqlServer = new azure.postgresql.Server("exampleServer", {
+    location: resourceGroup.location,
+    resourceGroupName: resourceGroup.name,
+    skuName: "B_Gen5_2",
+    storageMb: 100,
+    geoRedundantBackupEnabled: false,
+    administratorLogin: dbLogin,
+    administratorLoginPassword: dbPassword,
+    version: "9.5",
+    sslEnforcementEnabled: true,
+});
 
 // Create an Azure resource (Storage Account)
 // const account = new azure.storage.Account('storage', {
